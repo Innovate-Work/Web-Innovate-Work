119b78d8d111d9973908159d7ba52e08947773dd
artemPashevich <100874372+artemPashevich@users.noreply.github.com>
2023-10-07 13:50:18 -0700

    add button

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
 window.addEventListener('scroll', function() {
-    const header = document.getElementById('mainHeader');
+    const header = document.getElementById('staticHeader');
     if (window.scrollY > 0) {
         header.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.1)';
     } else {
         header.style.boxShadow = 'none';
     }
 });
+
