868347902b6bce853df182593669f339525e8f6e
artemPashevich <100874372+artemPashevich@users.noreply.github.com>
2023-10-10 23:47:19 -0700

    container with animation ready

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ button.addEventListener('click', function() {
     this.classList.add('pressed');
 });
 
+
+
+// Функция обратного вызова для наблюдателя
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('animate-in');
+            observer.unobserve(entry.target); // Прекратить наблюдение за этим элементом, так как анимация уже произошла
+        }
+    });
+}
+
+// Создаем наблюдатель
+const observer = new IntersectionObserver(handleIntersection, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1 // начать когда хотя бы 10% элемента видны
+});
+
+// Начнем наблюдение за каждой ячейкой
+const cells = document.querySelectorAll('.cell');
+cells.forEach(cell => {
+    observer.observe(cell);
+});
+
