101860f230131ea0e325161e81fb5ad5d8fd9ffe
artemPashevich <100874372+artemPashevich@users.noreply.github.com>
2023-10-30 22:53:56 -0700

    fix header add email link

--- scripts/general.js
+++ scripts/general.js
@@ -10,19 +10,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    window.addEventListener('scroll', function() {
-        const header = document.getElementById('staticHeader');
-        if (header) {
-            if (window.scrollY > 0) {
-                header.style.boxShadow = '0px 4px 10px rgba(0, 0, 0, 0.1)';
-            } else {
-                header.style.boxShadow = 'none';
-            }
-        }
-    });
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     var modal = document.getElementById('myModal');
     var btns = document.querySelectorAll('.next-button');
